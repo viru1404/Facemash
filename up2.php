a04b7f58f462d283b1b0531192c02933e6781000
viru <virendrasingh1404@gmail.com>
2015-09-10 14:15:37 -0700

    Update and rename home.php to up2.php

---
+++ up2.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+$pp=$_POST['name'];
+echo $pp."<br>";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "UPDATE Myguests SET score=score+1 WHERE id=$pp";
+
+if ($conn->query($sql) === TRUE) {
+	
+    header("location: aa.php");
+
+    
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+
+$conn->close();
+?>
