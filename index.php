68470bb722394dca8f7fad66d93fdca869da2761
viru <virendrasingh1404@gmail.com>
2015-09-23 17:04:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
 </head>
  
 <body>
-<div style="width:100%;height:80px;border:1px solid black;">
+<div style="width:100%;height:80px;border:1px solid black;background: orange;">
 
 <center>
 
-<h1>Facemash - Hot Or Not</h1>
-</div>
-<div style="width:100%;height:480px;border:1px solid black;">
+<h1>Facemash - Hot Or Not</h1></center>
 
-<div style="width:64%;height:470px;border:1px solid black;float:left;">
-<center><h3>Choose Whose Better</h3></center>
+
+</div>
+<div style="width:100%;height:480px;border:0px solid black;">
+
+<div style="width:64%;height:470px;border:0px solid black;float:left;">
+<center><h2>Choose Whose Better</h2>Click on any picture to Vote Up</center><br/>
 <?php
 $servername = "localhost";
 $username = "root";
@@ -35,20 +37,34 @@ if ($result->num_rows > 0) {
     echo"<center><table><tr>";
     while($row = $result->fetch_assoc()) {
     	
-        echo "<td width='400px'><center><br/><img src='".$row["avtar"]."' width='200px' height='300px'><br>";
-                echo  $row["firstname"]. " " . $row["lastname"]."<br/>Current Score :- ".$row["score"]." <br/><br/><form action='up2.php' method='post'><input type='text' value='".$row["id"]."' name='name' align='left' hidden><input type='submit' value='Vote Up'></form></center></td>";
+        echo "<td width='400px'><center>";
+                echo  "
+                <form action='up2.php' method='post'>
+                <input type='text' value='".$row["id"]."' name='name' align='left' hidden>
+                <input type='submit' value=' '  style='border:0px solid black;width:200px;height:300px;background:url(";
+                	echo '"'.$row["avtar"].'"';
+                	echo");background-size: 200px 300px;
+    background-repeat: no-repeat;'>
+                </form>
+                ".$row["firstname"]. " " . $row["lastname"]."<br/>Current Score :- ".$row["score"]." 
+               </center></td>";
 
     }
     echo"</tr></table></center>";
 } else {
     echo "Database Error";
 }
+echo"<center>";
+echo"<a href='index.php' style='text-decoration:none;float:right'>Skip</a>";
+
+echo"</center>";
+
 
 $conn->close();
 ?>
 
 </div>
-<div style="width:32%;height:470px;border:1px solid black;float:right;">
+<div style="width:32%;height:480px;border-left:1px solid black;float:right;">
 <center>
 <h1>#Trending People</h1>
 
@@ -68,22 +84,29 @@ $sql="SELECT * FROM myguests ORDER BY score DESC LIMIT 5 ";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+	echo"<br><br>";
     echo"<table style='border:0px solid black;margin-top:0px;'>
     <tr style='border:0px solid black;width:100%;'>
-    <td style='border-bottom:1px solid black;width:300;'>Avtaar</td>
-    <td style='border-bottom:1px solid black;width:30%;'>Name</td>
-    <td style='border-bottom:1px solid black;width:30%;'>Score</td>
+    <td style='border-bottom:0px solid black;width:300;'>Avtaar</td>
+    <td style='border-bottom:0px solid black;width:30%;'>Name</td>
+    <td style='border-bottom:0px solid black;width:30%;'>Score</td>
     </tr>";
     while($row = $result->fetch_assoc()) {
+
     	
       	echo "<tr>";
          echo "
          <td style='border:0px solid black;'><img src=' ". $row["avtar"]. "' width='50px' height='50px'> </td>
 		 <td style='border:0px solid black;'>" . $row["firstname"]. " " . $row["lastname"]." </td>
          <td style='border:0px solid black;'>".$row["score"]. "</td>
-         <br>";
+         ";
     }
     echo"</table>";
+    echo"<center>";
+echo"<br><br><a href='lists.php' style='text-decoration:none'>Show all</a>";
+
+echo"</center>";
+
 } else {
     echo "0 results";
 }
@@ -94,10 +117,16 @@ $conn->close();
 
 </div>
 </div>
-<div style="width:100%;height:80px;border:1px solid black;"><br/><br/>
+<div style="width:100%;height:80px;border:1px solid black;">
 <center>
-&copy; RAPL Ind
+
+
+<h3><center><a href='uploadnow.php' style='text-decoration:none;float:right'>upload Ur Better Image <br></a></center></h3>
+
+<h3><bold><br>Mail us at :: fightercode32@gmail.com
+</bold></h3>
 </center>
+
 
 
 </div>
