e74c24f72c0b89cbffd7ac7e0916e652d4fadbda
viru <virendrasingh1404@gmail.com>
2015-09-23 17:24:44 +0530

    Create uploadnow.php

---
+++ uploadnow.php
@@ -0,0 +1,54 @@
+
+<html>
+<head>
+<title>Facemash - Hot Or Not </title>
+<link href="ex.css" type="css/text" rel="stylesheet">
+</head>
+ 
+<body>
+<div style="width:100%;height:80px;border:1px solid black;background: orange;">
+
+<center>
+
+<h1>Facemash - Hot Or Not</h1></center>
+
+
+</div>
+<div style="width:100%;height:480px;border:0px solid black;">
+
+<div style="width:64%;height:470px;border:0px solid black;float:left;">
+<center><h2>Upload Ur Image</h2>Get started : Name your own name to image before uploading </center><br/>
+
+
+<center>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:<br><br>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+</center>
+
+
+</div>
+<div style="width:32%;height:480px;border-left:0px solid black;float:right;">
+<center>
+
+</center>
+
+</div>
+</div>
+<div style="width:100%;height:80px;border:1px solid black;">
+<center>
+
+
+<h3><center><a href='index.php' style='text-decoration:none;float:right'>Back TO FACEMASH <br></a></center></h3>
+
+<h3><bold><br>Mail us at :: fightercode32@gmail.com
+</bold></h3>
+</center>
+
+
+
+</div>
+</body>
+</html>
